4a9a3929081d690a0ba6ab06ca5185fabc157050
Daria Khrushch <73955564+Daria-Khrushch@users.noreply.github.com>
2022-11-23 10:17:59 +0000

    section30
    tests

--- script2.js
+++ script2.js
@@ -14,13 +14,12 @@ const getPeoplePromise = (fetch) => {
 const getPeople = async (fetch) => {
   const getRequest = await fetch('http://swapi.py4e.com/api/people');
   const data =  await getRequest.json();
-  // console.log(data);
   return {
     count: data.count,
     results: data.results
   };
 }
-// getPeople();
+// getPeople(fetch);
 // getPeoplePromise();
 
 module.exports = {
